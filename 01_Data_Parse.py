6b7827d7198b650629ba28f4db59cdc15db1e0fc
denglish83 <104326100+denglish83@users.noreply.github.com>
2022-05-05 10:22:04 -0400

    Add files via upload
    Changed all variables to be '_' insead of ' ' so i could upload to MySQLDB without issues.  Also, now writes to a MySQLDB!  Also, I made a MySQLDB

--- 01_Data_Parse.py
+++ 01_Data_Parse.py
@@ -1,7 +1,12 @@
 import pandas as pd
+from sqlalchemy import create_engine
+import pymysql
+import urllib
+import mysql.connector
+from mysql.connector import Error
+import numpy as np
 
 pd.set_option('display.max_columns', None)
-
 
 def parse_season(year):
     paris = parse_tour(1, 'Paris', year)
@@ -24,93 +29,167 @@ def parse_season(year):
     vuelta = parse_tour(14, 'Vuelta', year)
     lombardy = parse_one_day(15, 'Il_Lombardia', year)
 
-    merge1 = pd.merge(paris, tireno, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Tireno'))
-    merge2 = pd.merge(merge1, milan, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Milan'))
-    merge3 = pd.merge(merge2, flanders, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Flanders'))
-    merge4 = pd.merge(merge3, liege, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Liege'))
-    merge5 = pd.merge(merge4, giro, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Giro'))
-    merge6 = pd.merge(merge5, dauphine, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Dauphine'))
-    merge7 = pd.merge(merge6, tdf, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'TDF'))
-    merge8 = pd.merge(merge7, vuelta, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Vuelta'))
-    merge9 = pd.merge(merge8, lombardy, how = "outer", on=["First Name", "Last Name"], suffixes = ('', 'Lombardy'))
+    merge1 = pd.merge(paris, tireno, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Tireno'))
+    merge2 = pd.merge(merge1, milan, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Milan'))
+    merge3 = pd.merge(merge2, flanders, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Flanders'))
+    merge4 = pd.merge(merge3, liege, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Liege'))
+    merge5 = pd.merge(merge4, giro, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Giro'))
+    merge6 = pd.merge(merge5, dauphine, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Dauphine'))
+    merge7 = pd.merge(merge6, tdf, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_TDF'))
+    merge8 = pd.merge(merge7, vuelta, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Vuelta'))
+    merge9 = pd.merge(merge8, lombardy, how = "outer", on=["First_Name", "Last_Name"], suffixes = ('', '_Lombardy'))
 
     if year != 2020:
-        merge10 = pd.merge(merge9, catalunya, how="outer", on=["First Name", "Last Name"], suffixes=('', 'Catalunya'))
-        merge11 = pd.merge(merge10, basque, how="outer", on=["First Name", "Last Name"], suffixes=('', 'Basque'))
-        merge12 = pd.merge(merge11, roubaix, how="outer", on=["First Name", "Last Name"], suffixes=('', 'Roubaix'))
-        merge13 = pd.merge(merge12, romandie, how="outer", on=["First Name", "Last Name"], suffixes=('', 'Romandie'))
-        merge14 = pd.merge(merge13, suisse, how="outer", on=["First Name", "Last Name"], suffixes=('', 'Suisse'))
+        merge10 = pd.merge(merge9, catalunya, how="outer", on=["First_Name", "Last_Name"], suffixes=('', '_Catalunya'))
+        merge11 = pd.merge(merge10, basque, how="outer", on=["First_Name", "Last_Name"], suffixes=('', '_Basque'))
+        merge12 = pd.merge(merge11, roubaix, how="outer", on=["First_Name", "Last_Name"], suffixes=('', '_Roubaix'))
+        merge13 = pd.merge(merge12, romandie, how="outer", on=["First_Name", "Last_Name"], suffixes=('', '_Romandie'))
+        merge14 = pd.merge(merge13, suisse, how="outer", on=["First_Name", "Last_Name"], suffixes=('', '_Suisse'))
         final = merge14.copy()
-        final['Age'] = final[['Age', 'AgeTireno', 'AgeMilan', 'AgeCatalunya', 'AgeFlanders', 'AgeBasque', 'AgeRoubaix',
-                              'AgeLiege', 'AgeRomandie', 'AgeGiro', 'AgeDauphine', 'AgeSuisse', 'AgeTDF', 'AgeVuelta',
-                              'AgeLombardy']].bfill(axis=1).iloc[:, 0]
-        final['Team'] = final[['Team', 'TeamTireno', 'TeamMilan', 'TeamCatalunya', 'TeamFlanders', 'TeamBasque',
-                               'TeamRoubaix', 'TeamLiege', 'TeamRomandie', 'TeamGiro', 'TeamDauphine', 'TeamSuisse',
-                               'TeamTDF', 'TeamVuelta', 'TeamLombardy']].bfill(axis=1).iloc[:, 0]
+        final['Age'] = final[['Age', 'Age_Tireno', 'Age_Milan', 'Age_Catalunya', 'Age_Flanders', 'Age_Basque', 'Age_Roubaix',
+                              'Age_Liege', 'Age_Romandie', 'Age_Giro', 'Age_Dauphine', 'Age_Suisse', 'Age_TDF', 'Age_Vuelta',
+                              'Age_Lombardy']].bfill(axis=1).iloc[:, 0]
+        final['Team'] = final[['Team', 'Team_Tireno', 'Team_Milan', 'Team_Catalunya', 'Team_Flanders', 'Team_Basque',
+                               'Team_Roubaix', 'Team_Liege', 'Team_Romandie', 'Team_Giro', 'Team_Dauphine', 'Team_Suisse',
+                               'Team_TDF', 'Team_Vuelta', 'Team_Lombardy']].bfill(axis=1).iloc[:, 0]
         final = final.drop(
-            ['AgeTireno', 'AgeMilan', 'AgeCatalunya', 'AgeFlanders', 'AgeBasque', 'AgeRoubaix', 'AgeLiege',
-             'AgeRomandie', 'AgeGiro', 'AgeDauphine', 'AgeSuisse', 'AgeTDF', 'AgeVuelta', 'AgeLombardy'], axis=1)
+            ['Age_Tireno', 'Age_Milan', 'Age_Catalunya', 'Age_Flanders', 'Age_Basque', 'Age_Roubaix', 'Age_Liege',
+             'Age_Romandie', 'Age_Giro', 'Age_Dauphine', 'Age_Suisse', 'Age_TDF', 'Age_Vuelta', 'Age_Lombardy'], axis=1)
         final = final.drop(
-            ['TeamTireno', 'TeamMilan', 'TeamCatalunya', 'TeamFlanders', 'TeamBasque', 'TeamRoubaix', 'TeamLiege',
-             'TeamRomandie', 'TeamGiro', 'TeamDauphine', 'TeamSuisse', 'TeamTDF', 'TeamVuelta', 'TeamLombardy'], axis=1)
+            ['Team_Tireno', 'Team_Milan', 'Team_Catalunya', 'Team_Flanders', 'Team_Basque', 'Team_Roubaix', 'Team_Liege',
+             'Team_Romandie', 'Team_Giro', 'Team_Dauphine', 'Team_Suisse', 'Team_TDF', 'Team_Vuelta', 'Team_Lombardy'], axis=1)
     else:
         final = merge9.copy()
-        final['Age'] = final[['Age', 'AgeTireno', 'AgeMilan', 'AgeFlanders', 'AgeLiege', 'AgeGiro', 'AgeDauphine', 'AgeTDF', 'AgeVuelta', 'AgeLombardy']].bfill(axis=1).iloc[:, 0]
-        final['Team'] = final[['Team', 'TeamTireno', 'TeamMilan', 'TeamFlanders', 'TeamLiege', 'TeamGiro', 'TeamDauphine', 'TeamTDF', 'TeamVuelta', 'TeamLombardy']].bfill(axis=1).iloc[:, 0]
-        final = final.drop(['AgeTireno', 'AgeMilan', 'AgeFlanders', 'AgeLiege', 'AgeGiro', 'AgeDauphine', 'AgeTDF', 'AgeVuelta', 'AgeLombardy'], axis=1)
-        final = final.drop(['TeamTireno', 'TeamMilan', 'TeamFlanders', 'TeamLiege','TeamGiro', 'TeamDauphine', 'TeamTDF', 'TeamVuelta', 'TeamLombardy'], axis=1)
+        final['Age'] = final[['Age', 'Age_Tireno', 'Age_Milan', 'Age_Flanders', 'Age_Liege', 'Age_Giro', 'Age_Dauphine', 'Age_TDF', 'Age_Vuelta', 'Age_Lombardy']].bfill(axis=1).iloc[:, 0]
+        final['Team'] = final[['Team', 'Team_Tireno', 'Team_Milan', 'Team_Flanders', 'Team_Liege', 'Team_Giro', 'Team_Dauphine', 'Team_TDF', 'Team_Vuelta', 'Team_Lombardy']].bfill(axis=1).iloc[:, 0]
+        final = final.drop(['Age_Tireno', 'Age_Milan', 'Age_Flanders', 'Age_Liege', 'Age_Giro', 'Age_Dauphine', 'Age_TDF', 'Age_Vuelta', 'Age_Lombardy'], axis=1)
+        final = final.drop(['Team_Tireno', 'Team_Milan', 'Team_Flanders', 'Team_Liege','Team_Giro', 'Team_Dauphine', 'Team_TDF', 'Team_Vuelta', 'Team_Lombardy'], axis=1)
 
-    final = final.rename(columns={"Overall Rank":"Overall RankParis", "Time Dif":"Time DifParis", "Points Rank":"Points RankParis", "Points":"PointsParis", "Mountains Rank":"Mountains RankParis", "Mountains Points":"Mountains PointsParis"})
-    final = final.sort_values(by = ['Last Name', 'First Name'])
+        final['Overall_Rank_Catalunya'] = np.nan
+        final['Time_Dif_Catalunya'] = np.nan
+        final['Points_Rank_Catalunya'] = np.nan
+        final['Points_Catalunya'] = np.nan
+        final['Mountains_Rank_Catalunya'] = np.nan
+        final['Mountains_Points_Catalunya'] = np.nan
+
+        final['Overall_Rank_Basque'] = np.nan
+        final['Time_Dif_Basque'] = np.nan
+        final['Points_Rank_Basque'] = np.nan
+        final['Points_Basque'] = np.nan
+        final['Mountains_Rank_Basque'] = np.nan
+        final['Mountains_Points_Basque'] = np.nan
+
+        final['Overall_Rank_Roubaix'] = np.nan
+        final['Time_Dif_Roubaix'] = np.nan
+
+        final['Overall_Rank_Romandie'] = np.nan
+        final['Time_Dif_Romandie'] = np.nan
+        final['Points_Rank_Romandie'] = np.nan
+        final['Points_Romandie'] = np.nan
+        final['Mountains_Rank_Romandie'] = np.nan
+        final['Mountains_Points_Romandie'] = np.nan
+
+        final['Overall_Rank_Suisse'] = np.nan
+        final['Time_Dif_Suisse'] = np.nan
+        final['Points_Rank_Suisse'] = np.nan
+        final['Points_Suisse'] = np.nan
+        final['Mountains_Rank_Suisse'] = np.nan
+        final['Mountains_Points_Suisse'] = np.nan
+
+    final = final.rename(columns={"Overall_Rank":"Overall_Rank_Paris", "Time_Dif":"Time_Dif_Paris", "Points_Rank":"Points_Rank_Paris", "Points":"Points_Paris", "Mountains_Rank":"Mountains_Rank_Paris", "Mountains_Points":"Mountains_Points_Paris"})
+
+    final = final.sort_values(by = ['Last_Name', 'First_Name'])
     final['Season'] = year
     final.reset_index(inplace = True, drop = True)
     print(final.shape)
-    print(final.head())
+    #print(final.head())
+
+    sqlEngine = create_engine('mysql+pymysql://root:k2udaeV%40@localhost:3306/cycling', pool_recycle=3600)
+    connection = sqlEngine.connect()
+
+    try:
+        frame = final.to_sql(f"season_{year}", connection, if_exists='fail');
+
+    except ValueError as vx:
+        print(vx)
+    except Exception as ex:
+        print(ex)
+    else:
+        print(f"Table season_{year} created successfully.");
+    finally:
+        connection.close()
+
 
 def parse_one_day(race_no, race_name, year):
     file_name = f'/Users/USER/Documents/Cycling/Data/{year}/{race_no}_{race_name}.xlsx'
     results = pd.read_excel(file_name)
-    results = results[['First Name', 'Last Name', 'Age', 'Team', 'Rank', 'Result']]
+    results['First_Name'] = results['First Name']
+    results['Last_Name'] = results['Last Name']
+    results['Overall_Rank'] = results['Rank']
+    results['Time_Dif'] = results['Result']
+    results = results[['First_Name', 'Last_Name', 'Age', 'Team', 'Overall_Rank', 'Time_Dif']]
+    results['Overall_Rank'] = results['Overall_Rank'].fillna(999)
     return results
 
 def parse_tour(race_no, race_name, year):
     file_name = f'/Users/USER/Documents/Cycling/Data/{year}/{race_no}_1_{race_name}_Start_list.xlsx'
     start = pd.read_excel(file_name)
-    start = start[['First Name', 'Last Name', 'Age', 'Team']]
+    start['First_Name'] = start['First Name']
+    start['Last_Name'] = start['Last Name']
+    start = start[['First_Name', 'Last_Name', 'Age', 'Team']]
 
     file_name = f'/Users/USER/Documents/Cycling/Data/{year}/{race_no}_2_{race_name}_Overall.xlsx'
     overall = pd.read_excel(file_name)
-    overall['Overall Rank'] = overall['Rank']
-    overall['Time Dif'] = overall['Result']
-    overall = overall[['First Name', 'Last Name', 'Overall Rank', 'Time Dif']]
+    overall['Overall_Rank'] = overall['Rank']
+    overall['Time_Dif'] = overall['Result']
+    overall['First_Name'] = overall['First Name']
+    overall['Last_Name'] = overall['Last Name']
+    overall = overall[['First_Name', 'Last_Name', 'Overall_Rank', 'Time_Dif']]
 
     if race_no != 4 or year not in (2017, 2018):
         file_name = f'/Users/USER/Documents/Cycling/Data/{year}/{race_no}_3_{race_name}_Points.xlsx'
         points = pd.read_excel(file_name)
-        points['Points Rank'] = points['Rank']
+        points['Points_Rank'] = points['Rank']
         points['Points'] = points['Result']
-        points = points[['First Name', 'Last Name', 'Points Rank', 'Points']]
+        points['First_Name'] = points['First Name']
+        points['Last_Name'] = points['Last Name']
+        points = points[['First_Name', 'Last_Name', 'Points_Rank', 'Points']]
 
     file_name = f'/Users/USER/Documents/Cycling/Data/{year}/{race_no}_4_{race_name}_Mountains.xlsx'
     mountains = pd.read_excel(file_name)
-    mountains['Mountains Rank'] = mountains['Rank']
-    mountains['Mountains Points'] = mountains['Result']
-    mountains = mountains[['First Name', 'Last Name', 'Mountains Rank', 'Mountains Points']]
+    mountains['First_Name'] = mountains['First Name']
+    mountains['Last_Name'] = mountains['Last Name']
+    mountains['Mountains_Rank'] = mountains['Rank']
+    mountains['Mountains_Points'] = mountains['Result']
+    mountains = mountains[['First_Name', 'Last_Name', 'Mountains_Rank', 'Mountains_Points']]
 
 
-    inter1 = pd.merge(start, overall, how = "left", on=["First Name", "Last Name"])
+    inter1 = pd.merge(start, overall, how = "left", on=["First_Name", "Last_Name"])
     if race_no != 4 or year not in (2017, 2018):
-        inter2 = pd.merge(inter1, points, how = "left", on=["First Name", "Last Name"])
-        final = pd.merge(inter2, mountains, how = "left", on=["First Name", "Last Name"])
+        inter2 = pd.merge(inter1, points, how = "left", on=["First_Name", "Last_Name"])
+        final = pd.merge(inter2, mountains, how = "left", on=["First_Name", "Last_Name"])
     else:
-        final = pd.merge(inter1, mountains, how="left", on=["First Name", "Last Name"])
+        final = pd.merge(inter1, mountains, how="left", on=["First_Name", "Last_Name"])
+        final['Points_Rank'] = np.nan
+        final['Points'] = np.nan
+
+    final['Overall_Rank'] = final['Overall_Rank'].fillna(999)
 
     return final
 
-#parse_season(2015)
-#parse_season(2016)
-#parse_season(2017)
-#parse_season(2018)
-#parse_season(2019)
+#parse_season(2009)
+parse_season(2010)
+parse_season(2011)
+parse_season(2012)
+parse_season(2013)
+parse_season(2014)
+parse_season(2015)
+parse_season(2016)
+parse_season(2017)
+parse_season(2018)
+parse_season(2019)
 parse_season(2020)
-#parse_season(2021)
+parse_season(2021)
+
+
+
